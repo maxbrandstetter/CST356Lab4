f22890b4efa156e61bd0ba3b2eea148377df8254
Max Brandstetter <middlestetter999@gmail.com>
2016-10-28 16:02:40 -0700

    Finished lab

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+﻿function submit() {
+    var firstName = document.getElementById("fname").value;
+    var lastName = document.getElementById("lname").value;
+    var age = document.getElementById("age").value;
+
+    document.getElementById("firstnamespan").innerHTML = firstName;
+    document.getElementById("lastnamespan").innerHTML = lastName;
+    document.getElementById("agespan").innerHTML = age;
+}
